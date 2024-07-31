5e5affd3982ed923110458ceca6826e031ddfb59
James Riley Wynne III <wynnejr@ornl.gov>
2024-07-31 14:10:44 -0400

    Adding chmod to buildscript

--- build.sh
+++ build.sh
@@ -16,4 +16,5 @@ else
 	conda activate ${DEV_DEST}
 	pip3 install -r requirements.txt
 	python3 ./setup.py install --install-scripts ${DEV_DEST}/bin/hsi_xfer
+	chmod 770 /sw/sources/hpss/hsi-xfer/bin/hsi_xfer/hsi_xfer
 fi
