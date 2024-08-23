8e98ad0c51af41515cf01cc94f21e509347f487e
James Riley Wynne III <wynnejr@ornl.gov>
2024-08-23 10:35:32 -0400

    Bumping to python 3.12

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ DEV_DEST=${PWD}/hsi-xfer-dev-env
 PROD_DEST=/sw/sources/hpss/hsi-xfer
 
 if [[ "${1:-x}" == "prod" ]] ; then
-	conda create -y -p ${PROD_DEST} python=3.9
+	conda create -y -p ${PROD_DEST} python=3.12
 	source activate ${PROD_DEST}
 	conda activate ${PROD_DEST}
 	pip3 install -r requirements.txt
