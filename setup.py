d6c29d9e4fb0c362323a9aee922e045f87e759de
James Riley Wynne III <wynnejr@ornl.gov>
2024-09-16 11:38:41 -0400

    Bumping version to 1.2.0

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ class CustomInstall(install):
         super().run()
 
 setup(name='hsi_xfer',
-      version='1.1.0',
+      version='1.2.0',
       description='A wrapper for HSI that enforces ordered recall from tape, and increases ease of use. Think: rsync for HPSS',
       author='hsi_xfer',
       author_email='wynnejr@ornl.gov',
       url='https://gitlab.ccs.ornl.gov/archive-team/hsi_xfer',
       license='UNLICENSED',
       zip_safe=False,
-      python_requires='>=3.7',
+      python_requires='>=3.9',
       packages=find_packages(),
       entry_points={
           'console_scripts': [
